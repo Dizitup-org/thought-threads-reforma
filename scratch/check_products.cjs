9f994fad2f65b86c8f106bee36a5a433f7f7da8e
Abhraleen Roy <abhra135@gmail.com>
2026-05-12 21:54:55 +0530

    adrijo's shundor mistake db link in string exposed

--- scratch/check_products.cjs
+++ scratch/check_products.cjs
@@ -2,7 +2,7 @@ const pg = require('pg');
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_xW6moP8efztF@ep-polished-frost-anj12h57-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL
 });
 
 async function checkProducts() {
